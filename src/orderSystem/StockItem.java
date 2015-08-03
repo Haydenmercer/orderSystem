63ee3327a99b5f002590e387801e80a423813a15
= <hmercer@PC-CZC45100J0.netbuilder.private>
2015-08-03 15:19:45 +0100

    first commit

---
+++ src/orderSystem/StockItem.java
@@ -0,0 +1,13 @@
+package orderSystem;
+
+public class StockItem {
+
+	private int id;
+	private String name; 
+	private String description;
+	private int price;
+	private int stockQuantity;
+	private int poop;
+	
+	
+}
